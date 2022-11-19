cb51988137bbff07deb2510fabcbe4664f5e9343
Andrii Vasin <bewerlyproduction@gmail.com>
2022-11-19 16:21:48 +0200

    🦄 refactor(Refactor): Refactoring

--- bricks/simple_repository/__brick__/test/repositories/{{repository_name.snakeCase()}}_repository_test.dart
+++ bricks/simple_repository/__brick__/test/repositories/{{repository_name.snakeCase()}}_repository_test.dart
@@ -1,15 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:erasmus_m_c/repositories/{{repository_name.pascalCase()}}/{{repository_name.pascalCase()}}.dart';
-
-class MockApi{{repository_name.pascalCase()}}Repository extends Mock implements Api{{repository_name.pascalCase()}}Repository {}
 
 void main() {
-  final Api{{repository_name.pascalCase()}}Repository mockApi{{repository_name.pascalCase()}}Repository =
-      MockApi{{repository_name.pascalCase()}}Repository();
   group("Testing {{repository_name.pascalCase()}} Repository", () {
-    //TODO: implement the tests 
-    
+    //TODO: implement the tests
+
     setUp(() {});
     test("Test method", () {});
   });
