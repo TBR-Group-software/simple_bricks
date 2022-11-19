877769c34dec66be4497bf8b18b19a2583e6a6c6
Andrii Vasin <bewerlyproduction@gmail.com>
2022-11-19 16:26:43 +0200

    🦄 refactor(Refactor): Brick

--- bricks/simple_repository/__brick__/lib/repositories/{{repository_name.snakeCase()}}/api_{{repository_name.snakeCase()}}_repository/api_{{repository_name.snakeCase()}}_repository.dart
+++ bricks/simple_repository/__brick__/lib/repositories/{{repository_name.snakeCase()}}/api_{{repository_name.snakeCase()}}_repository/api_{{repository_name.snakeCase()}}_repository.dart
@@ -1,10 +1,9 @@
+import 'package:erasmus_m_c/repositories/{{repository_name.snakeCase()}}/{{repository_name.snakeCase()}}.dart';
+
+
 /// {@template api_{{repository_name.snakeCase()}}_repository}
 /// [{{repository_name.pascalCase()}}Repository]
 /// {@endtemplate}
 class Api{{repository_name.pascalCase()}}Repository extends {{repository_name.pascalCase()}}Repository {
   /// {@macro api_{{repository_name.snakeCase()}}_repository}
-  Api{{repository_name.pascalCase()}}Repository({
-  });
-
-
 }
